b9e6b859094ad4a8a5a01f938a8349c9da853c42
moh-hosseini98 <moh.hosseini1998@gmail.com>
2025-03-27 19:08:50 +0330

    add loginUser Service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,11 @@
-import { Injectable,ConflictException, HttpException, } from '@nestjs/common';
+import { Injectable,ConflictException, HttpException,NotFoundException,UnauthorizedException } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { User } from '@prisma/client';
 import { PrismaService } from 'src/core/services/prisma.service';
 import { hash,compare } from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
+import { LoginUserDto } from './dtos/login-user.dto';
+import { LoginResponse, UserPayload } from 'src/interfaces/users-login.interface';
 
 @Injectable()
 export class UsersService {
@@ -38,7 +40,32 @@ export class UsersService {
         }
       }
 
-    async loginUser() {
+    async loginUser(loginUserDto : LoginUserDto) : Promise<LoginResponse> {
+        try {
+            const user = await this.prisma.user.findUnique({
+                where : {email : loginUserDto.email}
+            })
+
+            if (!user) {
+                throw new NotFoundException('User not found');
+            }
+            if (!(await compare(loginUserDto.password, user.password))) {
+                throw new UnauthorizedException('Invalid credentials');
+            }
+            const payload: UserPayload = {
+                // create payload for JWT
+                sub: user.id, // sub is short for subject. It is the user id
+                email: user.email,
+                name: user.name,
+            }
+
+            return {
+                // return access token
+                access_token: await this.jwtService.signAsync(payload),
+            }
+        } catch(error) {
+            throw new HttpException(error, 500);
+        }
 
     }
 
