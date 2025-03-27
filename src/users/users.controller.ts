a7af077debb9ba318f19eb06aab46390989101bc
moh-hosseini98 <moh.hosseini1998@gmail.com>
2025-03-27 18:10:09 +0330

    main functions for users controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { LoginUserDto } from './dtos/login-user.dto';
+import { UpdateUsertDto } from './dtos/update-user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+    @Post('register')
+    registerUser(@Body() createUserDto : CreateUserDto): string {
+        return "Register User"
+    }
+
+    @Post('login')
+    loginUser(@Body() loginUserDto : LoginUserDto): string {
+        return "Login User"
+    }
+
+    @Get('me')
+    me(): string {
+        return "Get Profile"
+    }
+
+    @Patch(':id')
+    updateUser(
+        @Param('id',ParseIntPipe) id :number,
+        @Body() updateUserDto: UpdateUsertDto
+    ): string {
+        return `Update user ${id}`
+    }
+
+    @Delete(':id')
+    deleteUser(@Param('id', ParseIntPipe) id: number): string {
+        return `Delete User ${id}!`;
+    }
+  
+}
